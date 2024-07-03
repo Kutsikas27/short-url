c7b1ee30be7abdfd4f62a214aab6560eff158113
Kutsikas27 <mardus80@gmail.com>
2024-07-03 14:58:06 +0300

    uhuu

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+//@ts-nocheck
+
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 // Basic Configuration
@@ -8,17 +10,26 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
+app.use(express.json());
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
-
+const urlDatabase = {};
+let nextShortUrlId = 1;
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post("/api/shorturl", function (req, res) {
+  const originalUrl = req.body.url;
+  console.log(req.body);
+  const shortUrl = nextShortUrlId.toString();
+  urlDatabase[shortUrl] = originalUrl;
+
+  nextShortUrlId++;
+
+  res.json({ original_url: req.body.url, short_url: shortUrl });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
